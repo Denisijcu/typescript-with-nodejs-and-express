3aaad670c25ef639fe654b1c1adbea259f686e5c
Denis Sanchez Leyva <denisijcu266@gmail.com>
2019-05-19 22:37:39 -0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 const app: Application = express();
 
 const add = (a: number, b: number): number => a + b;
+let total = add(4,5);
 
 app.get('/', (req, res) => {
   const s = `<div> 
-
+                  <h1> Test : ${total}
     <h1>Cabecera</h1>
     <p>Title</p>
     <table>
